e5dcc7864cb0e394848ce1114b5912f282bb3b72
ljc1301 <ljc1301@126.com>
2017-07-13 16:30:50 +0800

    Create lambda_and_function_pointer.cpp

---
+++ CPP/lambda_and_function_pointer.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <cstdlib>
+double reduce(double(*func)(double,double),double *begin,double *end)
+{
+	double ans=*begin;
+	for(begin++;begin!=end;begin++)
+		ans=func(ans,*begin);
+	return ans;
+}
+int main()
+{
+	int i,n;
+	double *a;
+	scanf("%d",&n);
+	a=(double *)malloc(sizeof(double)*n);
+	for(i=0;i<n;i++) scanf("%lf",a+i);
+	printf("%lf\n",reduce([](double a,double b){ return a+b; },a,a+n));
+	return 0;
+}
