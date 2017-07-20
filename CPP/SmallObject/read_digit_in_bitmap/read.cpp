8693956e548b051d6b69610d19a392da6065f82c
ljc1301 <ljc1301@126.com>
2017-07-20 16:30:10 +0800

    Update read.cpp

--- CPP/SmallObject/read_digit_in_bitmap/read.cpp
+++ CPP/SmallObject/read_digit_in_bitmap/read.cpp
@@ -3,20 +3,21 @@
 #include <cmath>
 #include <algorithm>
 using namespace std;
-struct number
-{
-	int num;
-	double p;
-	bool operator <(const number& n) const { return p>n.p; }
-}numbers[10];
 typedef struct
 {
     BYTE b;
     BYTE g;
     BYTE r;
 }RGB;
+const int max_lable_num=100;
+struct number
+{
+	char lable[100];
+	double p;
+	bool operator <(const number& n) const { return p>n.p; }
+}numbers[max_lable_num];
 const int r=5,c=3;
-int height,width,i,j,a,cnt[r*c];
+int height,width,i,j,a,cnt[r*c],lnum;
 RGB img[1500][1500];
 double sum[r*c],t,add,psum;
 char name[100];
@@ -27,7 +28,7 @@ FILE *pic,*data;
 double sigma(double x) { if(exp(-x)==1/1.0) return 0; return 1/(1+exp(-x)); }
 int main()
 {
-	system("get_data");
+	//system("get_data");
 	printf("Please input bitmap path(Ctrl+Z to end): ");
 	while(scanf("%s",name)==1)
 	{
@@ -58,18 +59,19 @@ int main()
 					}
 				for(i=0;i<r*c;i++) sum[i]/=cnt[i];
 	    		data=fopen("data","r");
-	    		psum=0;
-				for(i=0;i<10;i++)
+	    		lnum=0; psum=0;
+				for(i=0;fscanf(data,"%s",numbers[i].lable)==1;i++,lnum++)
 				{
 					add=0;
 					for(j=0;j<r*c;j++)
 						fscanf(data,"%lf",&t),add+=t*sum[j];
-					numbers[i].num=i; numbers[i].p=sigma(add);
+					numbers[i].p=sigma(add);
 					psum+=sigma(add);
 				}
-				for(i=0;i<10;i++) numbers[i].p/=psum;
-				sort(numbers,numbers+10);
-				for(i=0;i<10;i++) printf("%d: %-3.1lf%%\n",numbers[i].num,100*numbers[i].p);
+				for(i=0;i<lnum;i++) numbers[i].p/=psum;
+				sort(numbers,numbers+lnum);
+				for(i=0;i<lnum;i++)
+					printf("%10s: %.1lf%%\n",numbers[i].lable,100*numbers[i].p);
 				fclose(data);
 		    }
 		    fclose(pic);
