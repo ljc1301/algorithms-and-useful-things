922017ed4ffa0dff33fc4cc247d83936d802c2fc
ljc1301 <ljc1301@126.com>
2018-08-22 09:41:40 +0800

    Create duipai.cpp

---
+++ CPP/SmallObject/duipai.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int bigrand() { return rand()|(rand()<<15); }
+long long hugerand() { return bigrand()|((bigrand()+0ll)<<30); }
+FILE *f;
+#define printf(format,args...) fprintf(f,format,##args)
+void make(int p)
+{
+	//Input the code
+}
+#undef printf
+int main()
+{
+	srand(time(NULL));
+    do
+    {
+        f=fopen("test.in","w");
+        make();
+        fclose(f);
+        system("test < test.in > test.out");
+        system("std < test.in > test.ans");
+    }while(!system("fc test.out test.ans"));
+	return 0;
+}
