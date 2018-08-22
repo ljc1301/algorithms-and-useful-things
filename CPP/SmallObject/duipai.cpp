82d2b70e4308667ecc090f0d7009adfb4974ac72
ljc1301 <ljc1301@126.com>
2018-08-22 18:53:27 +0800

    Update duipai.cpp

--- CPP/SmallObject/duipai.cpp
+++ CPP/SmallObject/duipai.cpp
@@ -4,14 +4,14 @@ int bigrand() { return rand()|(rand()<<15); }
 long long hugerand() { return bigrand()|((bigrand()+0ll)<<30); }
 FILE *f;
 #define printf(format,args...) fprintf(f,format,##args)
-void make(int p)
+void make()
 {
-	//Input the code
+    //Input the code
 }
 #undef printf
 int main()
 {
-	srand(time(NULL));
+    srand(time(NULL));
     do
     {
         f=fopen("test.in","w");
@@ -20,5 +20,5 @@ int main()
         system("test < test.in > test.out");
         system("std < test.in > test.ans");
     }while(!system("fc test.out test.ans"));
-	return 0;
+    return 0;
 }
