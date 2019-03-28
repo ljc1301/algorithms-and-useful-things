aef69bb9f511f42aa153bbe1044d9f8ab8bd5a47
ljc1301 <ljc1301@126.com>
2019-03-28 22:19:50 +0800

    Create save.py

---
+++ Python/wav(sound)/save.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import wave
+import numpy as np
+# import scipy.signal as signal
+import sys
+
+nchannels,framerate = [int(i) for i in input().split()]
+
+# time = 10
+
+# # 产生10秒44.1kHz的100Hz - 1kHz的频率扫描波
+# t = np.arange(0, time, 1.0/framerate)
+# wave_data = signal.chirp(t, 100, time, 1000, method='linear') * 10000
+# wave_data = wave_data.astype(np.short)
+
+wave_data=np.array([])
+for channel in range(nchannels):
+    wave_data=np.append(wave_data,np.array([int(i) for i in input().split()]))
+wave_data=wave_data.astype(np.short)
+wave_data.shape=nchannels,-1
+wave_data=wave_data.T
+
+# 打开WAV文档
+f = wave.open(sys.argv[1], "wb")
+
+# 配置声道数、量化位数和取样频率
+f.setnchannels(nchannels)
+f.setsampwidth(2)
+f.setframerate(framerate)
+# 将wav_data转换为二进制数据写入文件
+f.writeframes(wave_data.tostring())
+f.close()
